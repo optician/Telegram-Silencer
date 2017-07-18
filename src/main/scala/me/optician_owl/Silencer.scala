794edfce5d275cad592b654c3ef2cde99d9480b9
Danila Matveev <usurname.r@gmail.com>
2017-07-18 22:02:23 +0300

    catch messages

--- src/main/scala/me/optician_owl/Silencer.scala
+++ src/main/scala/me/optician_owl/Silencer.scala
@@ -1,6 +1,6 @@
 package me.optician_owl
 
-import info.mukel.telegrambot4s.api.{Polling, TelegramBot}
+import info.mukel.telegrambot4s.api.{ChatActions, Polling, TelegramBot}
 import info.mukel.telegrambot4s.api.declarative.Commands
 
 import scala.io.Source
@@ -10,7 +10,7 @@ object Silencer {
   System.setProperty("BOT_TOKEN", "408189074:AAGtJQ7clw9eS9NES-sZxYWcA2ZTfyuULlU")
 
   def main(args: Array[String]): Unit = {
-    object SafeBot extends TelegramBot with Polling with Commands {
+    object SafeBot extends TelegramBot with Polling with Commands with ChatActions {
       // Use 'def' or 'lazy val' for the token, using a plain 'val' may/will
       // lead to initialization order issues.
       // Fetch the token from an environment variable or untracked file.
@@ -20,7 +20,13 @@ object Silencer {
 
       lazy val token = "408189074:AAGtJQ7clw9eS9NES-sZxYWcA2ZTfyuULlU"
 
-      onCommand("/hello") { implicit msg => reply("My token is SAFE!") }
+      onCommand("/hello") { implicit msg =>
+        reply("My token is SAFE!")
+      }
+      onMessage { msg =>
+        println(msg)
+        println(msg.text)
+      }
     }
 
     SafeBot.run()
