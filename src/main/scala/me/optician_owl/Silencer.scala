30339e434bb65b2ae4269cf7ed374bc8390fbeb4
Danila Matveev <usurname.r@gmail.com>
2017-07-18 22:28:25 +0300

    * catch edited messages * add simple stats

--- src/main/scala/me/optician_owl/Silencer.scala
+++ src/main/scala/me/optician_owl/Silencer.scala
@@ -2,12 +2,18 @@ package me.optician_owl
 
 import info.mukel.telegrambot4s.api.{ChatActions, Polling, TelegramBot}
 import info.mukel.telegrambot4s.api.declarative.Commands
+import info.mukel.telegrambot4s.models.{ChatType, Message}
 
+import scala.collection.mutable
 import scala.io.Source
 
 object Silencer {
 
+  // ToDo Hide key
   System.setProperty("BOT_TOKEN", "408189074:AAGtJQ7clw9eS9NES-sZxYWcA2ZTfyuULlU")
+
+  // ToDO persistence
+  private val stats: mutable.Map[Int, Int] = mutable.Map()
 
   def main(args: Array[String]): Unit = {
     object SafeBot extends TelegramBot with Polling with Commands with ChatActions {
@@ -20,13 +26,24 @@ object Silencer {
 
       lazy val token = "408189074:AAGtJQ7clw9eS9NES-sZxYWcA2ZTfyuULlU"
 
+      // Todo use State
+      // Todo is it possible to load group history
+      // ToDo is it possible to request list of administrators
+      // Todo add rules as some general approach
+      val react: Message => Unit = msg =>
+        if (msg.chat.`type` == ChatType.Group || msg.chat.`type` == ChatType.Supergroup) {
+          msg.from.foreach(u => stats += (u.id -> (stats.getOrElse(u.id, 0) + 1)))
+          println(stats)
+          println(msg)
+          println(msg.text)
+        }
+
       onCommand("/hello") { implicit msg =>
         reply("My token is SAFE!")
       }
-      onMessage { msg =>
-        println(msg)
-        println(msg.text)
-      }
+
+      onMessage (react)
+      onEditedMessage (react)
     }
 
     SafeBot.run()
