2579fe52459b38efbf277711a84c70aadcb863e8
Danila Matveev <usurname.r@gmail.com>
2017-08-27 10:07:47 +0300

    * Update StatsService test * Testing NoviceAndSpammer rule.

--- src/main/scala/me/optician_owl/silencer/model/Rule.scala
+++ src/main/scala/me/optician_owl/silencer/model/Rule.scala
@@ -7,21 +7,22 @@ trait Rule {
   def apply(facts: Facts): Verdict
 }
 
-object NoviceAndSpammer extends Rule {
+class NoviceAndSpammer(noviceBoundary: Int) extends Rule {
 
-  val userChatStatsMonoid: Monoid[UserChatStats] = Monoid[UserChatStats]
+  // ToDo Wrong responsibility. Rule shouldn't change statistics.
+  private val userChatStatsMonoid: Monoid[UserChatStats] = Monoid[UserChatStats]
 
   override def apply(facts: Facts): Verdict = {
     val chatStats = facts.userStats.chatStats.getOrElse(facts.chat.id, userChatStatsMonoid.empty)
 
     if (chatStats.joiningDttm.isDefined
         && facts.evidences.nonEmpty
-        && chatStats.amountOfMessages <= 3)
+        && chatStats.amountOfMessages <= noviceBoundary)
       Infringement(NonEmptyList(Spam, Nil))
     else Innocent
   }
 }
 
 object Rule {
-  val codex: List[Rule] = List(NoviceAndSpammer)
+  val codex: List[Rule] = List(new NoviceAndSpammer(3))
 }
